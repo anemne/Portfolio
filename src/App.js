b95a9b22f79ea346c1ccdb2f0a84b5b91cf2843f
Ane Ness <ane-ness@hotmail.no>
2022-07-15 01:21:46 +0200

    basic function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { Menu } from './components/Menu';
+import { TopHeader } from './components/TopHeader';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> 
+        <Menu> </Menu>
+        <TopHeader> </TopHeader>
       </header>
+      <main> 
+        <p> Dette er hoveddel</p>
+      </main>
     </div>
   );
 }
